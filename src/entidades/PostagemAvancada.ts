2b9e96121932e0f16416071dd1213772692cb008
luiz-david05 <decd393@gmail.com>
2023-12-07 11:26:49 -0300

    source: update code

--- src/entidades/PostagemAvancada.ts
+++ src/entidades/PostagemAvancada.ts
@@ -34,28 +34,4 @@ export class PostagemAvancada extends Postagem {
     diminuirVisualizacoes() {
         this._visualizacoesRestantes--;
     }
-
-    // private validaHashtags(): void {
-    //     if (this._hashtags.length > 5) {
-    //         throw new Error("Número máximo de hashtags excedido");
-    //     }
-
-    //     for (const hashtag of this._hashtags) {
-    //         if (hashtag.length > 100) {
-    //             throw new Error("Hashtag com mais de 100 caracteres");
-    //         }
-    //     }
-
-    //     if (this._hashtags.length == 0) {
-    //         throw new Error("Deve haver pelo menos uma hashtag");
-    //     }
-
-    //     if (this._hashtags.length != new Set(this._hashtags).size) {
-    //         throw new Error("Hashtags duplicadas");
-    //     }
-
-    //     if (this._hashtags.some((hashtag) => hashtag.indexOf("#") != 0)) {
-    //         throw new Error("Hashtags devem começar com #");
-    //     }
-    // }
 }
